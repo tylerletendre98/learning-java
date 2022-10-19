7273ee941dabc8caf611083872e6b9ef2beeb6c5
Tyler Letendre <tylerletendre98@gmail.com>
2022-10-19 09:55:46 -0400

    learing variables

---
+++ Variables.java
@@ -0,0 +1,8 @@
+class Variables {
+    static int age=2;
+    public static void main(String[] args){
+        
+        int age = 24;
+        System.out.println("I am " + age + " years old.");
+    }
+}
