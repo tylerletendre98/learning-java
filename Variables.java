39ac510caa4d967bdb0a9226bd705a1f106155cd
Tyler Letendre <tylerletendre98@gmail.com>
2022-10-19 10:36:41 -0400

    finished operators and intiger section

--- Variables.java
+++ Variables.java
@@ -1,8 +1,34 @@
 class Variables {
     static int age=2;
     public static void main(String[] args){
+        //can only change variable value if it is created before your main method
+        // int age = 24;
+
+        // age += 7;\
         
-        int age = 24;
-        System.out.println("I am " + age + " years old.");
+        // int age = 50;
+
+        // System.out.println(age >= 18 && age <= 40);
+
+        // boolean isStudent = false;
+        // boolean isLibraryMember = true;
+
+        // System.out.println(isStudent || isLibraryMember);
+
+        // int score = 0;
+        // int turns = 10;
+
+        // score++;
+        // turns--;
+
+        // System.out.println(score);
+        // System.out.println(turns);
+
+        int number = 55;
+
+        System.out.println(++number);
+
+
+        // System.out.println("I am " + age + " years old.");
     }
 }
