583acd0537d27252f4d4d0cb7876312af9a4ca90
Tyler Letendre <tylerletendre98@gmail.com>
2022-10-31 13:27:16 -0400

    finishing the arrays lessons

--- Arrays.java
+++ Arrays.java
@@ -4,8 +4,22 @@ class Array {
     public static void main(String[] args) {
         char vowels[] = {'a','e','i', 'o', 'u'};
 
-        vowels[2] = 't';
+        int startingIndex = 1;
+        int endingIndex = 4;
 
-        System.out.println(Arrays.toString(vowels));
+        int numbers[] = {1,2,3,4,5};
+
+        int copyOfNumbers[] = Arrays.copyOf(numbers, numbers.length);
+
+        Arrays.fill(numbers, 0);
+
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(copyOfNumbers));
+
+        // Arrays.fill(vowels,startingIndex,endingIndex, 'x');
+
+
+
+        // System.out.println(Arrays.toString(vowels));
     }
 }
